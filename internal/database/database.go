bf79350bf05fcc8d366a403c13ccb3d7436ab27f
Antonio-Lima <gutto1998@gmail.com>
2026-02-20 18:06:15 -0300

    Add database initialization and migration logic using GORM for PostgreSQL

---
+++ internal/database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"kura/internal/model" // Ajuste o caminho
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	dsn := "host=localhost user=kura_user password=kura_password dbname=kura_db port=5432 sslmode=disable"
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Falha ao conectar no banco de dados:", err)
+	}
+
+	err = DB.AutoMigrate(&model.User{}, &model.Category{}, &model.Transaction{})
+	if err != nil {
+		log.Fatal("Falha ao rodar Migrations:", err)
+	}
+
+	log.Println("Conexão com o banco estabelecida e tabelas sincronizadas!")
+}
