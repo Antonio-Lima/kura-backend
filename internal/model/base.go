367a3718d588fd73c08e369c29ff5c1c8e143791
Antonio-Lima <gutto1998@gmail.com>
2026-02-20 18:06:42 -0300

    Enhance Base and Transaction models with GORM tags for primary key and automatic timestamps

--- internal/model/base.go
+++ internal/model/base.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type Base struct {
-	ID        uint      `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
--- internal/model/transaction.go
+++ internal/model/transaction.go
@@ -10,12 +10,14 @@ const (
 type TransactionInput struct {
 	Amount      int64           `json:"amount" binding:"required,gt=0"`
 	Description string          `json:"description" binding:"required,max=100"`
-	Category    string          `json:"category"`
+	CategoryId  uint            `json:"category_id" binding:"required"`
 	Type        TransactionType `json:"type" binding:"required,oneof=income expense"`
 }
 
 type Transaction struct {
 	Base
-	UserId uint `json:"user_id"`
+	UserId   uint     `json:"-"`
+	User     User     `json:"user" gorm:"foreignKey:UserId"`
+	Category Category `json:"-" gorm:"foreignKey:CategoryId"`
 	TransactionInput
 }
