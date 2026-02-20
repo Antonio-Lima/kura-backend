35bd4d825cb8decfa635e4ac99e56b3aaa9195f8
Antonio-Lima <gutto1998@gmail.com>
2026-02-20 18:07:09 -0300

    Initialize database connection in the main API entry point

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"kura/internal/database"
 	"kura/internal/handler"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	database.Init()
+
 	r := gin.Default()
 
 	r.POST("/transactions", handler.CreateTransaction)
