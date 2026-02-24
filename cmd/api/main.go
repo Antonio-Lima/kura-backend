2abdb84c6bebcb9257dc355163abf70059d8da2a
Antonio-Lima <gutto1998@gmail.com>
2026-02-24 16:32:39 -0300

    Add environment variable loading and JWT_SECRET validation in main.go; integrate LoginUser route for user authentication.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"kura/internal/database"
 	"kura/internal/handler"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	_ = godotenv.Load()
+
+	if os.Getenv("JWT_SECRET") == "" {
+		log.Fatal("ERRO CRÍTICO: A variável JWT_SECRET não foi definida!")
+	}
+
 	database.Init()
 
 	r := gin.Default()
 
-	// r.POST("/transactions", handler.CreateTransaction)
-
 	r.POST("/user/register", handler.RegisterUser)
+	r.POST("/user/login", handler.LoginUser)
 
 	r.Run(":8080")
 }
