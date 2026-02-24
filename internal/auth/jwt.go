c00ca3f8ada3af2d68734c6864c11e2079547c1a
Antonio-Lima <gutto1998@gmail.com>
2026-02-24 16:32:02 -0300

    Add JWT token generation functionality with custom claims in a new jwt.go file

---
+++ internal/auth/jwt.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type CustomClaims struct {
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uuid.UUID) (string, error) {
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	if jwtSecret == "" {
+		return "", fmt.Errorf("JWT_SECRET não configurado")
+	}
+
+	claims := CustomClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   userID.String(),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(jwtSecret))
+}
